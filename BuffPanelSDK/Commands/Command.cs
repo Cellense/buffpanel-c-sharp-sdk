aec46f969aedbc9e7b50705287699a6c7a8a6e19
Osama Hassanein <player1os@gmail.com>
2016-12-12 20:37:38 +0100

    Add refactored infinario SDK

---
+++ BuffPanelSDK/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BuffPanel;
+
+namespace BuffPanel.Commands
+{
+
+    internal interface Command
+    {
+        void Execute(State state);
+    }
+
+}
---
+++ BuffPanelSDK/Logging/Level.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuffPanel.Logging
+{
+
+    public enum Level { Debug, Info, Warn, Error };
+
+}
